ca27bff500d9157e301119e9eb4a2b661633868a
sarastackhouse <sarapalacios7@gmail.com>
2020-10-31 20:51:51 +0100

    Implementing functions to pick random characters

--- script.js
+++ script.js
@@ -20,7 +20,7 @@ class CharacterFemale extends Character {
         super(image,filmName)
         this.actressName = actressName;
         this.characterFemaleName = characterFemaleName;   
-    }     
+    }  
 }
 
 // QUESTION CLASS: MALE & FEMALE
@@ -76,20 +76,37 @@ class AnswerFemale extends Answer {
 
 // FEMALE DATA SECTION: CHARACTERS & QUESTIONS
 
+characterFemaleArr = []
+questionFemaleArr = []
+answerFemaleArr = []
+
 const characterFemale1 = new CharacterFemale('/images/pretty_woman.jpg','Julia Roberts','Vivian Ward','Pretty Woman')
 const characterFemale2 = new CharacterFemale('/images/marylin_monroe.jpg','Marilyn Monroe','The Girl','La tentación vive arriba')
 const characterFemale3 = new CharacterFemale('/images/barbara.jpg','Geena Davis','Barbara','Beetlejuice')
+
+characterFemaleArr.push(characterFemale1);
+characterFemaleArr.push(characterFemale2);
+characterFemaleArr.push(characterFemale3);
 
 const QuestionFemale1 = new QuestionFemale('¿A qué actriz representa el funko de la imagen?','¿En qué película aparecía este personaje?','¿Cuál es el nombre que tenía este personaje en la película?')
 const QuestionFemale2 = new QuestionFemale('¿De qué famosa actriz se trata?','¿En qué película pudimos ver a este personaje?','¿Cuál es el nombre de este personaje en la película que aparece?')
 const QuestionFemale3 = new QuestionFemale('¿Quién es esta actriz?','¿Recuerdas el nombre de la película en la que aparecía este personaje?','¿En esta famosísima película, cómo se llamaba este personaje?')
 const QuestionFemale4 = new QuestionFemale('¿Quién es esta famosa actriz?','Para nota, ¿sabrías decir cuál era el nombre de la película en la que aparecía este personaje?','Haz memoria, ¿cual era el nombre de este personaje?')
 
+questionFemaleArr.push(QuestionFemale1);
+questionFemaleArr.push(QuestionFemale2);
+questionFemaleArr.push(QuestionFemale3);
+questionFemaleArr.push(QuestionFemale4);
+
 const AnswerFemale1 = new AnswerFemale('Susan Sarandon','Sentido y sensibilidad','Bella Swan')
 const AnswerFemale2 = new AnswerFemale('Sharon Stone','Con faldas y a lo loco','Clarice Starling')
 const AnswerFemale3 = new AnswerFemale('Glen Close','Marte','Bella Swan')
 const AnswerFemale4 = new AnswerFemale('Julie Andrews','Los Goonies','Katniss Everdreen')
 
+answerFemaleArr.push(AnswerFemale1);
+answerFemaleArr.push(AnswerFemale2);
+answerFemaleArr.push(AnswerFemale3);
+answerFemaleArr.push(AnswerFemale4);
 
 
 
@@ -105,11 +122,27 @@ const questionMale3 = new QuestionMale('¿De qué actor se trata?','¿Recuerdas 
 const questionMale4 = new QuestionMale('¿Quién es este famoso actor?','Para nota, ¿sabrías decir cuál era el nombre de la película en la que aparecía este personaje?','Haz memoria, ¿cual era el nombre de este personaje?')
 
 const AnswerMale1 = new AnswerMale('Chuck Norris','American Ganster','Marty McFly')
-const AnswerMale1 = new AnswerMale('Silvester Stalone','El club de la lucha','Derek Vinyard')
-const AnswerMale1 = new AnswerMale('Leonardo Di Caprio','Reservois dog','Alex DeLarge')
-const AnswerMale1 = new AnswerMale('Russel Crowe','Aterriza como puedas','Rhett Butler')
+const AnswerMale2 = new AnswerMale('Silvester Stalone','El club de la lucha','Derek Vinyard')
+const AnswerMale3 = new AnswerMale('Leonardo Di Caprio','Reservois dog','Alex DeLarge')
+const AnswerMale4 = new AnswerMale('Russel Crowe','Aterriza como puedas','Rhett Butler')
 
 
 
-console.log(characterFemale1)
-console.log(questionMale3)
+function selectRandom(arr){
+    let randomValue = Math.floor(Math.random()*arr.length)
+    return arr[randomValue];
+  }
+
+ 
+function picker(arr){
+    const picker = '';
+
+    picker.femaleCharacter = selectRandom(characterFemaleArr)
+    picker.femaleQuestion = selectRandom(characterFemaleArr)
+    picker.femalesAnswer = selectRandom(characterFemaleArr)
+
+    return picker 
+  }
+
+
+console.log(picker(arr))
